9d68d26336a96b9f40dc1aa7bee110972f563dd6
unchang3d <sp812bro@gmail.com>
2021-04-19 21:37:15 +0300

    test proj

---
+++ bot.py
@@ -0,0 +1,15 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler, CallbackContext
+from envs import TOKEN
+
+
+def hello(update: Update, context: CallbackContext) -> None:
+    update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+updater = Updater(TOKEN)
+
+updater.dispatcher.add_handler(CommandHandler('hello', hello))
+
+updater.start_polling()
+updater.idle()
