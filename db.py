f603bd87dfd8d643df6e4d5c9084b0cd02af7523
Gizzzka <61236535+Gizzzka@users.noreply.github.com>
2021-04-24 00:53:24 +0300

    Update db.py

--- db.py
+++ db.py
@@ -19,20 +19,23 @@ def init_bridges(data):
             params = [key, value]
             cur.execute(INSERT_BRIDGE, params)
 
-
+            
+def get_days():
+    year = datetime.now().year
+    month = datetime.now().month
+    days_number = (date(year, month + 1, 1) - date(year, month, 1)).days
+    
+    return days_number
+            
 def init_days():
     with sq.connect(DB_PATH) as con:
         cur = con.cursor()
-        
-        year = datetime.now().year
-        month = datetime.now().month
-        days_number = (date(year, month + 1, 1) - date(year, month, 1)).days
 
         date_rage_start = date.today()
         date_range_end = date_range_start + timedelta(month=1)
         date_range_step = timedelta(days=1)
 
-        while day <= day + timedelta(days=30):
+        while day <= day + timedelta(get_days()):
 
             cur.execute("""INSERT INTO day (ts) VALUES (?)""", day)
 
