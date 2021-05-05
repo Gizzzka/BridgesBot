cade7e93ec49f1a6f6547ac7d55076a43e839092
unchang3d <sp812bro@gmail.com>
2021-05-05 10:50:31 +0300

    parser improvements

--- envs.py
+++ envs.py
@@ -2,3 +2,13 @@
 
 DB_PATH = 'bridges_database.db'
 TOKEN = '1760883122:AAHNfTbFc2VGq130wolzSnoilAMNVkcpP6U'
+
+URL = 'https://mostotrest-spb.ru/'
+SEL_DETAILS = '/html/body/div[1]/div/div[1]/div[3]'
+
+FF_GECKO_PATH = '/Users/abarnett/Documents/geckodriver'
+# FF_GECKO_PATH = '/home/dmitry/Work/BridgesBot/geckodriver'
+FF_PREF_FLASH = 'dom.ipc.plugins.enabled.libflashplayer.so'
+FF_PREF_IMG = 'permissions.default.image'
+
+TIME_TEMPLATE = '%H:%M'
--- parser.py
+++ parser.py
@@ -1,83 +1,52 @@
-import time
-from datetime import datetime
-import random
-import json
+# -*- coding: utf-8 -*-
 from selenium import webdriver
-from pprint import pprint
-
-
-URL = 'https://mostotrest-spb.ru/'
-# GECKO_PATH = '/Users/abarnett/Documents/geckodriver'
-GECKO_PATH = '/home/dmitry/Work/BridgesBot/geckodriver'
+from datetime import datetime
+import time
+import envs
 
 
 def fix_title(title):
-    # fixing two-words titles
     if len(title.split(' ')) >= 2:
-        # defining final variable
         fixed_title = ''
-
-        # making all letters after the first lowercase
         for word in title.split(' '):
             fixed_title += word[0]
             fixed_title += word[1:].lower()
             fixed_title += ' '
-
-        # get rid of the last space
         fixed_title = 'Мост ' + fixed_title[:-1]
-
-    # fixing one-word titles
     else:
         fixed_title = title[0]
         fixed_title += title[1:].lower()
-
         fixed_title = fixed_title + ' мост'
-
     return fixed_title
 
 
 def fix_time(raw):
-    return datetime.strptime(raw, '%H:%M').time()
+    return datetime.strptime(raw, envs.TIME_TEMPLATE).time()
 
 
-def fix_schedule(wrong_schedule):
+def fix_schedule(wirings):
     result = {}
-
-    # fixing schedules with small time intervals
-    if len(wrong_schedule) > 2:
-        # adding bridge opening time
-        result[fix_time(wrong_schedule[-2])] = fix_time(wrong_schedule[0])
-
-        # adding small time intervals
-        if len(wrong_schedule) > 4:
-            result[fix_time(wrong_schedule[2])] = fix_time(wrong_schedule[3])
-
-        # adding bridge closing time
-        result[fix_time(wrong_schedule[-3])] = fix_time(wrong_schedule[-1])
-
-    elif len(wrong_schedule) == 2:
-        result[fix_time(wrong_schedule[0])] = fix_time(wrong_schedule[1])
-
-    # if argument is too short
-    else:
-        result = {}
-
+    if len(wirings) > 2:
+        result[fix_time(wirings[-2])] = fix_time(wirings[0])
+        if len(wirings) > 4:
+            result[fix_time(wirings[2])] = fix_time(wirings[3])
+        result[fix_time(wirings[-3])] = fix_time(wirings[-1])
+    elif len(wirings) == 2:
+        result[fix_time(wirings[0])] = fix_time(wirings[1])
     return [result]
 
 
 def configure_the_browser():
     fo = webdriver.FirefoxOptions()
+    fo.add_argument('--headless')
 
-    fo.add_argument('--headless')  # enable silent mode
-
-    fp = webdriver.FirefoxProfile()  # disable images load
-    fp.set_preference('permissions.default.image', 2)
-    fp.set_preference('dom.ipc.plugins.enabled.libflashplayer.so', 'false')
+    fp = webdriver.FirefoxProfile()
+    fp.set_preference(envs.FF_PREF_IMG, 2)
+    fp.set_preference(envs.FF_PREF_FLASH, 'false')
     fp.update_preferences()
 
-    driver = webdriver.Firefox(executable_path=GECKO_PATH,
-                               firefox_profile=fp,
-                               options=fo)
+    driver = webdriver.Firefox(executable_path=envs.FF_GECKO_PATH,
+                               firefox_profile=fp, options=fo)
     return driver
 
 
@@ -85,29 +54,23 @@ def get_schedule():
     driver = configure_the_browser()
     bridge_dict = {}
     try:
-        driver.get(URL)
+        driver.get(envs.URL)
         time.sleep(2)
-        driver.find_element_by_xpath(
-            '/html/body/div[1]/div/div[1]/div[3]'
-        ).click()  # selecting full schedule
+        driver.find_element_by_xpath(envs.SEL_DETAILS).click()
         time.sleep(2)
-        # finding tag <div> with all bridges tags
-        bridges_divs = driver.find_elements_by_class_name('bridge')
-        # iterating throw each bridge tag and filling the dict
-        for bridge in bridges_divs:
+        for bridge in driver.find_elements_by_class_name('bridge'):
             bridge_name = bridge.find_element_by_class_name('name').text
             bridge_name = fix_title(bridge_name)
             bridge_time = []
             for half_time_p in bridge.find_elements_by_tag_name('span'):
                 bridge_time.append(half_time_p.text)
-            bridge_time = fix_schedule(bridge_time)  # fixing the bridge time
-            bridge_dict[bridge_name] = bridge_time  # filling the dict
-    except Exception as ex:
-        print(ex)
+            bridge_dict[bridge_name] = fix_schedule(bridge_time)
+    except Exception as e:
+        print(e)
     driver.close()
     driver.quit()
     return bridge_dict
 
 
-# data = get_schedule()
-# pprint(data)
+data = get_schedule()
+print(data)
