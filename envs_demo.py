92c7d058165569ad11f8f9be9a7a630d5041c964
Gizzzka <61236535+Gizzzka@users.noreply.github.com>
2021-05-11 23:03:04 +0300

    Update envs_demo.py

--- envs_demo.py
+++ envs_demo.py
@@ -5,8 +5,8 @@ URL = 'https://mostotrest-spb.ru/'
 SEL_DETAILS = '/html/body/div[1]/div/div[1]/div[3]'
 #
 #
-# FF_GECKO_PATH = '/Users/abarnett/Documents/geckodriver'
-FF_GECKO_PATH = 'C:/Users/iljus/Desktop/Actual code/Trying selenium/geckodriver.exe'
+FF_GECKO_PATH = '/Users/abarnett/Documents/geckodriver'
+# FF_GECKO_PATH = 'C:/Users/iljus/Desktop/Actual code/Trying selenium/geckodriver.exe'
 FF_PREF_FLASH = 'dom.ipc.plugins.enabled.libflashplayer.so'
 FF_PREF_IMG = 'permissions.default.image'
 #
